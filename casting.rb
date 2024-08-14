b32d128e04c8f429ace096ab7b43653d3b7d6686
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-08-14 12:23:35 -0300

    casting

---
+++ casting.rb
@@ -0,0 +1,27 @@
+# Integer to float
+puts 1.to_f
+
+# Float to integer
+puts 1.0.to_i
+
+# String to integer
+puts "1".to_i
+
+# String to float
+puts "1".to_f
+
+# Integer to string
+puts 1.to_s
+
+# Float to string
+puts 1.0.to_s
+
+# Summary
+# to_f: Converts to float
+# to_i: Converts to integer
+# to_s: Converts to string
+# to_a: Converts to array
+# to_h: Converts to hash
+# to_sym: Converts to symbol
+# to_c : Converts to complex number
+# to_r: Converts to rational number
