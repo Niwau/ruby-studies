e87f3c8fae0adddf54de9157f30a1abec5f60f43
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-08-14 12:12:36 -0300

    math

---
+++ math.rb
@@ -0,0 +1,47 @@
+a = 9
+b = 2
+
+puts "Addition of 9 and 2:"
+puts a + b
+
+puts "Subtraction of 9 and 2:"
+puts a - b
+
+puts "Multiplication of 9 and 2:"
+puts a * b
+
+puts "Division of 9 and 2:"
+puts a / b
+
+puts "Modulus of 9 and 2:"
+puts a % b
+
+puts "Exponentiation of 9 and 2:"
+puts a ** b
+
+puts ""
+
+puts "Float operations"
+
+# IMPORTANT: Ruby will return a float if any of the operands is a float
+
+a = 9.0
+b = 2.0
+
+puts "Addition of 9.0 and 2.0:"
+puts a + b
+
+puts "Subtraction of 9.0 and 2.0:"
+puts a - b
+
+puts "Multiplication of 9.0 and 2.0:"
+puts a * b
+
+puts "Division of 9.0 and 2.0:"
+puts a / b
+
+puts "Modulus of 9.0 and 2.0:"
+puts a % b
+
+puts "Exponentiation of 9.0 and 2.0:"
+puts a ** b
